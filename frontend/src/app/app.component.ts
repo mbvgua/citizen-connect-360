c33002c1b36ab4cfe27c11fbf8d388b2cbb3eb3f
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-10 12:50:35 +0300

    :construction: build the homepage structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { RegisterEmailComponent } from "./register-email/register-email.component";
 import { LoginEmailComponent } from "./login-email/login-email.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { HomepageComponent } from "./homepage/homepage.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RegisterEmailComponent, LoginEmailComponent, ForgotPasswordComponent]
+    imports: [RouterOutlet, RegisterEmailComponent, LoginEmailComponent, ForgotPasswordComponent, HomepageComponent]
 })
 export class AppComponent {
   title = 'frontend';
