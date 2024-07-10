a0e92d4593b9c22b0e0e25de621546e6f08f190d
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-10 22:17:40 +0300

    :moyai: add the main structure and basic styling for auth pages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,13 +4,15 @@ import { RegisterEmailComponent } from "./register-email/register-email.componen
 import { LoginEmailComponent } from "./login-email/login-email.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
 import { HomepageComponent } from "./homepage/homepage.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { RegisterChoiceComponent } from "./register-choice/register-choice.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RegisterEmailComponent, LoginEmailComponent, ForgotPasswordComponent, HomepageComponent]
+    imports: [RouterOutlet, RegisterEmailComponent, LoginEmailComponent, ForgotPasswordComponent, HomepageComponent, NavbarComponent, RegisterChoiceComponent]
 })
 export class AppComponent {
   title = 'frontend';
