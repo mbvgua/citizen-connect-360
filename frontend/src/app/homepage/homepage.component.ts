663c13f5dc59d609823af7e1d2e9dfac87190930
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-12 23:02:35 +0300

    :8ball: properly style the hero homepage section

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+
+  constructor() {}
+
+  // worked on the typewriter effect in the hero page
+  typedText = document.querySelector('.second-text') as HTMLElement
+
+  textLoad(){
+    setTimeout(()=>{
+      this.typedText.textContent = 'voice'
+    },0)
+
+    setTimeout(()=>{
+      this.typedText.textContent = 'power'
+    },4000)
+
+    setTimeout(()=>{
+      this.typedText.textContent = 'platform'
+    },8000)
+  }
+  
+  
+  ngOnInit(): void {
+    // call the type writer word change function to esnure it runs
+    // this.textLoad()
+    
+    // console.log(this.typedText.textContent)
+  }
 
 }
