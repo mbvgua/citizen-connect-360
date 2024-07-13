e5c077e27ebadb735deab29548706e55324e159d
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-13 18:07:22 +0300

    :book: add the popup forms functionality
    get summary and add view will have a popup form on clicking on the views page

--- frontend/src/app/views/views.component.ts
+++ frontend/src/app/views/views.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-views',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './views.component.html',
   styleUrl: './views.component.css'
 })
-export class ViewsComponent {
+export class ViewsComponent implements OnInit,OnChanges{
+
+  constructor() {}
+
+  form!: FormGroup
+
+
+  // function for update popup
+  addView(): void {
+    const popup = document.getElementById("popup-1")! as HTMLDivElement
+    if (popup) {
+      popup.classList.toggle("active");
+    }
+  }
+
+  // function for getting the summary popup
+  getSummary(): void {
+    const popup = document.getElementById("popup-2")! as HTMLDivElement
+    if (popup) {
+      popup.classList.toggle("active");
+    }
+  }
+
+
+  
+    ngOnInit(): void {
+      
+    }
+    
+    ngOnChanges(changes: SimpleChanges): void {
+      // create and remove the update popup
+      this.addView()
+      this.getSummary()
+      
+    }
 
 }
