f1a0c8c627eef06d2d0b862ab5e52a62a63cfb6c
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-13 18:23:27 +0300

    :snowflake: add the incidents page for both citizens and officials
    similar to the views page. will finetune with specifics later. what will be in the form. how summary is gotten and such

--- frontend/src/app/incidents/incidents.component.ts
+++ frontend/src/app/incidents/incidents.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-incidents',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './incidents.component.html',
   styleUrl: './incidents.component.css'
 })
-export class IncidentsComponent {
+export class IncidentsComponent implements OnChanges{
+
+  constructor() {}
+
+  form!: FormGroup
+
+
+  // function for update popup
+  addIncident(): void {
+    const popup = document.getElementById("popup-1")! as HTMLDivElement
+    if (popup) {
+      popup.classList.toggle("active");
+    }
+  }
+
+  // function for getting the summary popup
+  getSummary(): void {
+    const popup = document.getElementById("popup-2")! as HTMLDivElement
+    if (popup) {
+      popup.classList.toggle("active");
+    }
+  }
+
+
+  
+    ngOnInit(): void {
+      
+    }
+    
+    ngOnChanges(changes: SimpleChanges): void {
+      // create and remove the update popup
+      this.addIncident()
+      this.getSummary()
+      
+    }
 
 }
