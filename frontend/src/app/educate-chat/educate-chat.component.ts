7dfb7ff65c7cf5893fe46df72f7ecabef19a670e
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-14 15:05:38 +0300

    :anchor: add the educate and the chatbot page
    to add the sidebar section later. proved quite challenging to make it responsive

--- frontend/src/app/educate-chat/educate-chat.component.ts
+++ frontend/src/app/educate-chat/educate-chat.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-educate-chat',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './educate-chat.component.html',
   styleUrl: './educate-chat.component.css'
 })
-export class EducateChatComponent {
+export class EducateChatComponent implements OnInit{
+
+  constructor() {}
+
+  // sidebar UI
+  // btn = document.querySelector('.toggle')! as HTMLElement
+  sidebar:any = document.querySelector('.sidebar')! as HTMLElement
+
+  displaySidebar(){
+    if (this.sidebar){
+      this.sidebar.classList.toggle('active')
+      // console.log('radaaa')
+    }
+  }
+
+  ngOnInit(): void {
+    
+    // display the sidebar code
+    this.displaySidebar()
+  }
+
 
 }
