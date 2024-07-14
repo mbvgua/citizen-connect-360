46ea3c5b0097465b006eb7cb97c3a1376fc5e891
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-14 19:18:05 +0300

    :golf: style the polls component

--- frontend/src/app/polls/polls.component.ts
+++ frontend/src/app/polls/polls.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-polls',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule,RouterModule],
   templateUrl: './polls.component.html',
   styleUrl: './polls.component.css'
 })
-export class PollsComponent {
+export class PollsComponent implements OnInit, OnChanges{
+
+
+  constructor() {}
+
+  form!: FormGroup
+
+
+  // function for update popup
+  addIncident(): void {
+    const popup = document.getElementById("popup-1")! as HTMLDivElement
+    if (popup) {
+      popup.classList.toggle("active");
+    }
+  }
+
+  submitPoll():void{
+    const popup = document.getElementById("popup-2")! as HTMLDivElement
+    if (popup) {
+      popup.classList.toggle("active");
+    }
+
+  }
+
+  
+    ngOnInit(): void {
+      
+    }
+    
+    ngOnChanges(changes: SimpleChanges): void {
+      // create and remove the update popup
+      this.addIncident()
+      
+    }
 
 }
