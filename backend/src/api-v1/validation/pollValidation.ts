a383c98d38c705ddd287e2373d082b6c94b150c6
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-21 02:49:54 +0300

    :person_fencing: add custom validation messages for all inputs

--- backend/src/api-v1/validation/pollValidation.ts
+++ backend/src/api-v1/validation/pollValidation.ts
@@ -1,12 +1,38 @@
 import Joi from 'joi'
 
 export const pollSchema = Joi.object({
-    title:Joi.string().required(),
-    description:Joi.string().required(),
-    userId:Joi.string().required(),
-    choices:Joi.array().items(Joi.string().regex(/^[^,]+$/).min(2).required()).required()
-    
-    // choices:Joi.array().required().messages({
-    //     'any.required':'we wacha bro'
-    // })
+    title:Joi.string().required().messages({
+        'string.base': 'Title should be a type of text',
+        'string.empty': 'Title is required',
+        'any.required': 'Title is required'
+      }),
+
+    description:Joi.string().required().messages({
+      'string.base': 'Description should be a type of text',
+      'string.empty': 'Description is required',
+      'any.required': 'Description is required'
+    }),
+
+    userId:Joi.string().required().messages({
+      'string.base': 'User ID should be a type of text',
+      'string.empty': 'User ID is required',
+      'any.required': 'User ID is required'
+    }),
+
+    choices: Joi.array().items(
+      Joi.string()
+        .regex(/^[^,]+$/) // Regex prevents having any commas within words.
+        .messages({
+          'string.pattern.base': 'Each choice should not contain commas'
+        })
+        .min(2) // Ensure there are at least 2 choices
+        .messages({
+          'array.items.min': 'At least 2 choices are required'
+        })
+        .required()
+    ).required().messages({
+      'array.base': 'Choices should be an array',
+      'array.empty': 'Choices array cannot be empty',
+      'any.required': 'Choices are required'
+    })
 })
