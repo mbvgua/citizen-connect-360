6bcbf44215a6f6553d6fc29e34e9bae072702a55
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-23 04:55:11 +0300

    enable cors to allow for communication with the frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,24 @@
-import express, {json} from 'express'
-import cors from 'cors'
-import authRouter from './api-v1/routes/authRoutes'
-import viewRouter from './api-v1/routes/viewRoutes'
-import incidentRouter from './api-v1/routes/incidentRoutes'
-import pollRouter from './api-v1/routes/pollRoutes'
-import voteRouter from './api-v1/routes/voteRoutes'
+import express, { json } from "express";
+import cors from "cors";
+import authRouter from "./api-v1/routes/authRoutes";
+import viewRouter from "./api-v1/routes/viewRoutes";
+import incidentRouter from "./api-v1/routes/incidentRoutes";
+import pollRouter from "./api-v1/routes/pollRoutes";
+import voteRouter from "./api-v1/routes/voteRoutes";
 
-const app = express()   // initialize the application
+const app = express(); // initialize the application
 
-app.use(json())          //add a body to the requests
-// app.use(cors()) //for communication with the frontend
-
+app.use(json()); //add a body to the requests
+app.use(cors()); //for communication with the frontend
 
 // add middleware and respective urls
-app.use("/auth", authRouter)
-app.use("/views", viewRouter)
-app.use("/incidents", incidentRouter)
-app.use("/polls", pollRouter)
-app.use("/votes",voteRouter)
-
+app.use("/auth", authRouter);
+app.use("/views", viewRouter);
+app.use("/incidents", incidentRouter);
+app.use("/polls", pollRouter);
+app.use("/votes", voteRouter);
 
 // start the application
-app.listen(4000,()=>{
-    console.log('Mashallah! The server is running...')
-})
+app.listen(4000, () => {
+  console.log("Mashallah! The server is running...");
+});
