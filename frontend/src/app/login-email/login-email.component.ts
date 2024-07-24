f80d550a24025bc19709b07d91e6a919e0a1a5e5
mbvgua <105845499+mbvgua@users.noreply.github.com>
2024-07-24 16:31:35 +0300

    add registration and login using NGRX

--- frontend/src/app/login-email/login-email.component.ts
+++ frontend/src/app/login-email/login-email.component.ts
@@ -5,6 +5,9 @@ import { AuthService } from '../services/authService/auth.service';
 import { Router } from '@angular/router';
 import { StatusCheckService } from '../services/authStatusCheck/status-check.service';
 import { Subscription } from 'rxjs';
+import { AppState } from '../state';
+import { Store } from '@ngrx/store';
+import { AuthActions } from '../state/actions/auth.actions';
 
 @Component({
   selector: 'app-login-email',
@@ -17,7 +20,8 @@ export class LoginEmailComponent implements OnInit{
 
   constructor ( 
     private auth:AuthService,
-    private status:StatusCheckService
+    private status:StatusCheckService,
+    private store:Store<AppState>
   ){}
 
   // create the form
@@ -31,52 +35,56 @@ export class LoginEmailComponent implements OnInit{
   passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,10}$/
 
 
-  onSubmit(){
-    this.auth.loginUser(this.form.value).subscribe(
-      (response) =>{
-        this.message = response.message   //message to be displayed in the DOM
+  // onSubmit(){
+  //   this.auth.loginUser(this.form.value).subscribe(
+  //     (response) =>{
+  //       this.message = response.message   //message to be displayed in the DOM
 
-        // // CHECK IF TOKENS ARE WORKING PROPERLY
-        // console.log(response.token)
-        // console.log(response.decodedToken.role)
-        // console.log(response.decodedToken.id)
-        // console.log(response.decodedToken.email)
+  //       // // CHECK IF TOKENS ARE WORKING PROPERLY
+  //       // console.log(response.token)
+  //       // console.log(response.decodedToken.role)
+  //       // console.log(response.decodedToken.id)
+  //       // console.log(response.decodedToken.email)
 
-        localStorage.setItem('token',response.token)
-        localStorage.setItem('id',response.decodedToken.id)
-        localStorage.setItem('role',response.decodedToken.role)
+  //       localStorage.setItem('token',response.token)
+  //       localStorage.setItem('id',response.decodedToken.id)
+  //       localStorage.setItem('role',response.decodedToken.role)
         
-        // if token exists, save it to the localStorage
-        if(response.token){
-          this.message = response.message
-          // save token received to system local storage
-          this.id = localStorage.getItem('id') as string 
-          this.role = localStorage.getItem('role') as string
+  //       // if token exists, save it to the localStorage
+  //       if(response.token){
+  //         this.message = response.message
+  //         // save token received to system local storage
+  //         this.id = localStorage.getItem('id') as string 
+  //         this.role = localStorage.getItem('role') as string
 
-          // if the token role is admin
-          if(this.role === 'admin'){
-            // navigate to the admin panel
+  //         // if the token role is admin
+  //         if(this.role === 'admin'){
+  //           // navigate to the admin panel
           
-            this.router.navigate(['/admin'])
-          } else { // if the token role is citizen/govmt-official
+  //           this.router.navigate(['/admin'])
+  //         } else { // if the token role is citizen/govmt-official
 
-            setTimeout(()=>{    //delayed to read message on DOM
-              // this.status.login()
-              this.status.showStatus()
-              this.router.navigate([`dashboard/`,this.id])  //navigate to dashboard with user id
-            }, 1000)
-          }
+  //           setTimeout(()=>{    //delayed to read message on DOM
+  //             // this.status.login()
+  //             this.status.showStatus()
+  //             this.router.navigate([`dashboard/`,this.id])  //navigate to dashboard with user id
+  //           }, 1000)
+  //         }
           
-        }
+  //       }
 
-      },
-      (error)=>{
-        console.log(error)
-        this.message = error.error.message
-        // this.message.style.backgroungColor = red
-      })
+  //     },
+  //     (error)=>{
+  //       console.log(error)
+  //       this.message = error.error.message
+  //       // this.message.style.backgroungColor = red
+  //     })
 
-      this.form.reset   //clear form once submitted
+  //     this.form.reset   //clear form once submitted
+  // }
+
+  onSubmit(){
+    this.store.dispatch(AuthActions.login({user:this.form.value}))
   }
 
 
--- frontend/src/app/state/effects/auth.effects.ts
+++ frontend/src/app/state/effects/auth.effects.ts
@@ -2,8 +2,9 @@ import { inject, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthService } from "../../services/authService/auth.service";
 import { AuthActions } from "../actions/auth.actions";
-import { concatMap, map } from "rxjs";
+import { catchError, concatMap, exhaustMap, map, of } from "rxjs";
 import { Router } from "@angular/router";
+import { StatusCheckService } from "../../services/authStatusCheck/status-check.service";
 
 
 @Injectable()
@@ -11,10 +12,15 @@ export class AuthEffects{
 
     constructor(
         private action$:Actions,
-        private auth:AuthService
+        private auth:AuthService,
+        private router: Router,
+        private status:StatusCheckService
     ) {}
 
-    router = inject(Router)
+    message:string = ''
+    id:string = ''
+    role:string = ''
+
 
     registerUser$=createEffect(()=>{
         return this.action$.pipe(
@@ -24,7 +30,10 @@ export class AuthEffects{
             // receive only input   from action reaching out to the service
             concatMap( request => this.auth.registerUser(request.user).pipe(
                     map( response => {
-                            this.router.navigate(["login"]);
+                            // delay redirection, allow user to read message
+                            setTimeout(()=>{
+                            this.router.navigate(['./auth/login'])
+                            }, 1500)
                             return AuthActions.registerSuccess({response:response})
                         }
                     )
@@ -33,4 +42,43 @@ export class AuthEffects{
         
     })
 
+
+    loginUser$ = createEffect(() => {
+        return this.action$.pipe(
+          ofType(AuthActions.login),
+          exhaustMap(({ user }) => this.auth.loginUser(user).pipe(
+            map(response => {
+              localStorage.setItem('token',response.token)
+              localStorage.setItem('id',response.decodedToken.id)
+              localStorage.setItem('role',response.decodedToken.role)
+    
+               // Store the userId separately for easy access
+            if (response.token) {
+                
+                this.message = response.message
+                // save token received to system local storage
+                this.id = localStorage.getItem('id') as string 
+                this.role = localStorage.getItem('role') as string
+                
+                // if the token role is admin
+                if(this.role === 'admin'){
+                    // navigate to the admin panel
+                    
+                    this.router.navigate(['/admin'])
+                } else { // if the token role is citizen/govmt-official
+                    
+                    setTimeout(()=>{    //delayed to read message on DOM
+                        // this.status.login()
+                        this.status.showStatus()
+                        this.router.navigate([`dashboard/`,this.id])  //navigate to dashboard with user id
+                    }, 1000)
+                }
+            }
+    
+              return AuthActions.loginSuccess({ response: response });
+            }),
+            catchError(error => of(AuthActions.loginFailure({ message: error.error.message })))
+        ))
+    );
+});
 }
